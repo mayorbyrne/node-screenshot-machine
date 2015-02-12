0ec2ab963fc2740f5b44d4fb27efe94dad7b892a
Kevin Moritz <kevinecor@gmail.com>
2015-02-12 11:50:51 -0600

    rename writeStream

--- test/mocks/writeStream.js
+++ test/mocks/writeStream.js
@@ -2,20 +2,20 @@ var util = require('util'),
   EventEmitter = require('events').EventEmitter;
 var Writable = require('readable-stream').Writable;
 
-function writableStream(options) {
+function writeStream(options) {
   var self = this;
   options = options || {};
   options.decodeStrings = false;
   Writable.call(this, options);
 }
 
-util.inherits(writableStream, Writable);
+util.inherits(writeStream, Writable);
 
-writableStream.prototype._write = function(chunk, encoding, done) {
+writeStream.prototype._write = function(chunk, encoding, done) {
   this.emit('testChunk', chunk);
   done(null);
 };
 
-module.exports = exports = function writableStreamFactory(options) {
-  return new writableStream(options);
+module.exports = exports = function writeStreamFactory(options) {
+  return new writeStream(options);
 };
